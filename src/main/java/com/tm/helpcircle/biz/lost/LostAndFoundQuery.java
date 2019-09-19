5cc9280ee87ae147c9504863256b0135a7df3dd6
xl <xl@tongmi.com>
2019-09-19 11:34:36 +0800

    youhua

--- src/main/java/com/tm/helpcircle/biz/lost/LostAndFoundQuery.java
+++ src/main/java/com/tm/helpcircle/biz/lost/LostAndFoundQuery.java
@@ -8,7 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.util.CollectionUtils;
+
 
 import java.util.Arrays;
 import java.util.List;
